8786cda72d2cf0a2daacbce9ca03d13c9e080824
zhangshuo <3014zhangshuo@gmail.com>
2020-07-03 15:34:08 +0800

    Fix skip_before_filter to skip_before_action.

--- app/controllers/feishu/messages_controller.rb
+++ app/controllers/feishu/messages_controller.rb
@@ -1,6 +1,6 @@
 module Feishu
   class MessagesController < ApplicationController
-    skip_before_filter :verify_authenticity_token
+    skip_before_action :verify_authenticity_token
 
     def receive
       render json: { challenge: challenge }
