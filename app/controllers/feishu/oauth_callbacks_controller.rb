2a104513b1c61e096b64b7729e83b98a45c29f84
zhangshuo <3014zhangshuo@gmail.com>
2020-07-03 16:30:22 +0800

    Obtain feishu app_access_token.

--- app/controllers/feishu/oauth_callbacks_controller.rb
+++ app/controllers/feishu/oauth_callbacks_controller.rb
@@ -12,13 +12,23 @@ module Feishu
       params[:code]
     end
 
-    def get_access_token
+    def app_access_token
       @get_access_token ||= begin
         r_params = {
           app_id: "cli_9e7688822c74500c",
           app_secret: "6HxTE1njNUCG5OsxEpESwdJbTGWpHnJT",
+        }
+        result = RestClient.post "https://open.feishu.cn/open-apis/auth/v3/app_access_token/internal/", r_params.to_json, {content_type: :json, accept: :json}
+        JSON.parse(result)["app_access_token"]
+      end
+    end
+
+    def get_access_token
+      @get_access_token ||= begin
+        r_params = {
+          app_access_token: app_access_token,
           grant_type: "authorization_code",
-          code: params[:code]
+          code: code
         }
         RestClient.post "https://open.feishu.cn/open-apis/authen/v1/access_token", r_params.to_json, {content_type: :json, accept: :json}
       end
