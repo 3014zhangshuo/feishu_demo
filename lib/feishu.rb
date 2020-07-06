7b45322d1a5cce8d7328ae90653e3807759d540e
zhangshuo <3014zhangshuo@gmail.com>
2020-07-06 16:09:32 +0800

    Fix autoload.

--- lib/feishu.rb
+++ lib/feishu.rb
@@ -1,5 +1,13 @@
 # frozen_string_literal: true
 
 module Feishu
+  autoload :Base, 'feishu/base'
+  autoload :Auth, 'feishu/auth'
   autoload :Oauth, 'feishu/oauth'
+
+  module Message
+    autoload :Base, 'feishu/message/base'
+    autoload :Card, 'feishu/message/card'
+    autoload :P2pChatCreate, 'feishu/message/p2p_chat_create'
+  end
 end
